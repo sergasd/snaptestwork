0bfca610f4bb60c69eddd25f6dc0f40cee729c49
sergasd <sergasd@gmail.com>
2014-11-19 16:53:06 +0500

    extract class application

---
+++ app/lib/Application.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace TestWork\lib;
+
+
+use TestWork\repository\GenreRepository;
+use TestWork\repository\VideoRepository;
+
+class Application
+{
+
+    private $config;
+
+    private $container;
+
+    private static $_container;
+
+    public function __construct($config)
+    {
+        $this->config = $config;
+        $this->container = self::$_container = $this->createContainer();
+    }
+
+    private function createContainer()
+    {
+        $container = new Container($this->config);
+        $container->add('router', function() use($container) {
+            return new Router($container);
+        });
+
+        $container->add('db', function() use($container) {
+            $dbHost = $container->getParam('db.host');
+            $dbName = $container->getParam('db.name');
+            $userName = $container->getParam('db.user');
+            $password = $container->getParam('db.password');
+
+            $pdo = new \PDO("mysql:host=$dbHost;dbname=$dbName", $userName, $password);
+            $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        });
+
+        $container->add('repository.genre', function() use($container){
+            return new GenreRepository($container->get('db'));
+        });
+
+        $container->add('repository.video', function() use($container){
+            $imagesOutputDir = __DIR__ . '/files/video';
+            return new VideoRepository(
+                $container->get('db'),
+                $container->get('repository.genre'),
+                $container->get('image_handler'),
+                $imagesOutputDir
+            );
+        });
+
+        $container->add('image_handler', function() use($container){
+            return new ImagicImageHandler();
+        });
+
+        return $container;
+    }
+
+    public function createResponse()
+    {
+        $route = array_key_exists('r', $_GET) ? $_GET['r'] : $this->container->getParam('defaultRoute');
+        $router = $this->container->get('router');
+        return $router->runController($route);
+    }
+
+    /**
+     * @return Container
+    */
+    public static function getContainer()
+    {
+        return self::$_container;
+    }
+
+} 
--- www/index.php
+++ www/index.php
@@ -13,43 +13,7 @@ function d($data){
 require '../vendor/autoload.php';
 $config = require '../app/config/config.php';
 
-$container = new TestWork\lib\Container($config);
-$container->add('router', function() use($container) {
-    return new \TestWork\lib\Router($container);
-});
-
-$container->add('db', function() use($container) {
-    $dbHost = $container->getParam('db.host');
-    $dbName = $container->getParam('db.name');
-    $userName = $container->getParam('db.user');
-    $password = $container->getParam('db.password');
-
-    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $userName, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $pdo;
-});
-
-$container->add('repository.genre', function() use($container){
-    return new TestWork\repository\GenreRepository($container->get('db'));
-});
-
-$container->add('repository.video', function() use($container){
-    $imagesOutputDir = __DIR__ . '/files/video';
-    return new TestWork\repository\VideoRepository(
-        $container->get('db'),
-        $container->get('repository.genre'),
-        $container->get('image_handler'),
-        $imagesOutputDir
-    );
-});
-
-$container->add('image_handler', function() use($container){
-    return new \TestWork\lib\ImagicImageHandler();
-});
-
-
-$route = array_key_exists('r', $_GET) ? $_GET['r'] : $container->getParam('defaultRoute');
-$router = $container->get('router');
-$response = $router->runController($route);
+$application = new TestWork\lib\Application($config);
+$response = $application->createResponse();
 
 echo $response;
