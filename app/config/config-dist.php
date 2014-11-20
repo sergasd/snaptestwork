6b52ea01168b46fa82585cd528ae07697776eb08
sergasd <sergasd@gmail.com>
2014-11-20 12:07:27 +0500

    add config-dist

---
+++ app/config/config-dist.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'params' => [
+        'db.host' => 'localhost',
+        'db.name' => 'testwork',
+        'db.user' => 'root',
+        'db.password' => '',
+        'db.charset' => 'utf8',
+
+        'defaultRoute' => 'video/index',
+        'basePath' => dirname(__DIR__), // базовый путь
+        'webPath' => dirname(dirname(__DIR__)) . '/www', // путь к public части приложения
+        'baseUrl' => '/testwork', // если приложение в подпапке, то тут имя папки, если нет - то оставить пустым
+    ],
+];
