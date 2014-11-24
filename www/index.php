b80ebbe4e20c77faade84d20e0f23e116b4f2778
serega <sergasd@gmail.com>
2014-11-24 11:40:30 +0500

    cleanup

--- www/index.php
+++ www/index.php
@@ -1,15 +1,5 @@
 <?php
 
-
-error_reporting(-1);
-ini_set('display_errors', '1');
-
-function d($data){
-    echo '<pre>';
-    print_r($data);
-    echo '</pre>';
-}
-
 require '../vendor/autoload.php';
 $config = require '../app/config/config.php';
 
